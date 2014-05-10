525399868bb8eb6322d6771fc2b9094a71b085c8
cburke496 <cburke496@gmail.com>
2014-05-10 19:46:04 -0400

    Created dataset from csv file

--- 7/clustering/chris/main.js
+++ 7/clustering/chris/main.js
@@ -1,7 +1,43 @@
-var sexes = ['Male','Female'];
-var educations = ['Preschool','1st-4th','5th-6th','7th-8th','9th','10th','11th','12th','HS-grad','Prof-school','Some-college','Assoc-acdm','Assoc-voc','Bachelors','Masters','Doctorate'];
-//See http://archive.ics.uci.edu/ml/machine-learning-databases/adult/old.adult.names
+var data = {};
 
-d3.csv("census.txt")
-    .row(function(d) {
-	return {Age: d.age, Sex: function(d.sex) {
+var sex2num = function(d) {
+    if (d == "Male") {
+	return 0;
+    } else {
+	return 1;
+    }
+};
+var spouse2num = function(d) {
+    if(d == "Married-civ-spouse") {
+	return 0;
+    } else {
+	return 1;
+    }
+};
+var race2num = function(d) {
+    if(d == "White") {
+	return 0
+    } else {
+	return 1;
+    }
+};
+var class2num = function(d) {
+    if(d == ">50K") {
+	return 0;
+    } else {
+	return 1;
+    }
+};
+
+d3.csv("census.data").row(function(d) {
+    return {
+	Age : parseInt(d["age"]),
+	Sex : sex2num(d["sex"]),
+	Education : parseInt(d["educationnum"]),
+	Spouse : spouse2num(d["maritalstatus"]),
+	Race : race2num(d["race"]),
+	Class : class2num(d["class"]) };
+}).get(function(error, rows) {
+    data = rows;
+    console.log(data);
+});
