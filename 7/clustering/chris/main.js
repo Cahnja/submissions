f91fb52827ca264fe620eb700adc29ef842c6f73
cburke496 <cburke496@gmail.com>
2014-05-10 20:46:40 -0400

    Successfully separated data into clusters

--- 7/clustering/chris/main.js
+++ 7/clustering/chris/main.js
@@ -1,5 +1,10 @@
-var data = {};
+var data = [];
+var centroids = [];
 
+
+var age2num = function(d) {
+    return d / 100;
+};
 var sex2num = function(d) {
     if (d == "Male") {
 	return 0;
@@ -7,6 +12,9 @@ var sex2num = function(d) {
 	return 1;
     }
 };
+var ed2num = function(d) {
+    return d / 16;
+}; 
 var spouse2num = function(d) {
     if(d == "Married-civ-spouse") {
 	return 0;
@@ -29,15 +37,43 @@ var class2num = function(d) {
     }
 };
 
+var getKeys = function(dict) {
+    return Object.keys(dict).map(function(key) {return dict[key];});
+}
+
+var dist = function(d, c) {
+    var zipped = _.zip(getKeys(d), getKeys(c));
+    var distssquared = _.map(zipped, function(data) {return (data[1]-data[0])*(data[1]-data[0]);});
+    var sum = d3.sum(distssquared);
+    return Math.sqrt(sum);
+}
+
+var minDist = function(d) {
+    var dists =[dist(d,centroids[0]),dist(d,centroids[1]),dist(d,centroids[2])];
+    return dists.indexOf(d3.min(dists));
+}
+		      
+
+var maptocentroids = function(d) {
+    _.map(d, function(data) {
+	data["centroid"] = minDist(data);
+	return data;});
+}
+
 d3.csv("census.data").row(function(d) {
     return {
-	Age : parseInt(d["age"]),
+	Age : age2num(parseInt(d["age"])),
 	Sex : sex2num(d["sex"]),
-	Education : parseInt(d["educationnum"]),
+	Education : ed2num(parseInt(d["educationnum"])),
 	Spouse : spouse2num(d["maritalstatus"]),
 	Race : race2num(d["race"]),
 	Class : class2num(d["class"]) };
 }).get(function(error, rows) {
     data = rows;
+    centroids = [rows[0], rows[1], rows[2]];
+    var learningData = data.slice(0, 25000);
+    var testData = data.slice(25000);
+    maptocentroids(learningData);
     console.log(data);
+    console.log(learningData);
 });
